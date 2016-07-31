b34a1d1bc42835702116562fcd62a1284bad71d8
Frank A. Krueger <fak@praeclarum.org>
2016-07-30 20:58:03 -0700

    Add Test project

---
+++ Csg.Test/SphereTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Csg.Test
+{
+	[TestFixture]
+	public class SphereTest
+	{
+		[Test]
+		public void Unit()
+		{
+			var sphere = Solids.Sphere(1);
+			Assert.AreEqual(72, sphere.Polygons.Count);
+			var p0 = sphere.Polygons[0];
+			Assert.GreaterOrEqual(p0.Plane.W, 0.9);
+			Assert.LessOrEqual(p0.Plane.W, 1.1);
+		}
+
+		[Test]
+		public void BigRadius()
+		{
+			var sphere = Solids.Sphere(1.0e12);
+			var p0 = sphere.Polygons[0];
+			Assert.GreaterOrEqual(p0.Plane.W, 0.9e12);
+			Assert.LessOrEqual(p0.Plane.W, 1.1e12);
+		}
+	}
+}
+
