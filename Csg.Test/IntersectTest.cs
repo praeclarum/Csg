66e46ec8380a2600ad6c0c0d1b0871f3d6f6faf3
Frank A. Krueger <fak@praeclarum.org>
2016-07-31 10:16:40 -0700

    Test Intersections

---
+++ Csg.Test/IntersectTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Csg.Test
+{
+	[TestFixture]
+	public class IntersectTest
+	{
+		[Test]
+		public void UnitSphere_UnitSphere()
+		{
+			var sphere1 = Solids.Sphere(1, new Vector3D(-0.5, 0, 0));
+			var sphere2 = Solids.Sphere(1, new Vector3D(0.5, 0, 0));
+			var r = sphere1.Intersect(sphere2);
+			Assert.AreEqual(32, r.Polygons.Count);
+			Assert.IsTrue(r.IsCanonicalized);
+			Assert.IsTrue(r.IsRetesselated);
+		}
+
+		[Test]
+		public void UnitSphere_NoOverlap_UnitSphere()
+		{
+			var sphere1 = Solids.Sphere(1, new Vector3D(-50, 0, 0));
+			var sphere2 = Solids.Sphere(1, new Vector3D(50, 0, 0));
+			var r = sphere1.Intersect(sphere2);
+			Assert.AreEqual(0, r.Polygons.Count);
+			Assert.IsTrue(r.IsCanonicalized);
+			Assert.IsTrue(r.IsRetesselated);
+		}
+	}
+}
+
