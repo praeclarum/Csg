51c7eeba883cd7c7e3ccf7a2025e6024172752d2
Frank A. Krueger <fak@praeclarum.org>
2016-08-03 18:00:58 -0700

    Add PerfTest

---
+++ Csg.PerfTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+using static Csg.Solids;
+
+namespace Csg.PerfTest
+{
+	public class Program
+	{
+		static void PrimeJit()
+		{
+			var sphere1 = Sphere(1000, new Vector3D(-500, 0, 0));
+			var sphere2 = Sphere(1000, new Vector3D(500, 0, 0));
+			var sub = Difference(sphere1, sphere2);
+			Debug.Assert(sub.Polygons.Count > 0);
+		}
+
+		class TestResult
+		{
+			public int Resolution;
+			public TimeSpan Time;
+			public int Polygons;
+			public int OutputPolygons;
+			public long Compares => (long)Polygons * (long)Polygons;
+		}
+
+		static TestResult TestRes(int res)
+		{
+			var sw = new Stopwatch();
+			var sphere1 = Sphere(new SphereOptions { Resolution = res, Radius = 1000, Center = new Vector3D(-500, 0, 0) });
+			var sphere2 = Sphere(new SphereOptions { Resolution = res, Radius = 1000, Center = new Vector3D(500, 0, 0) });
+			sw.Start();
+			var sub = Difference(sphere1, sphere2);
+			sw.Stop();
+			var subTime = sw.Elapsed;
+			return new TestResult
+			{
+				Resolution = res,
+				Polygons = sphere1.Polygons.Count,
+				OutputPolygons = sub.Polygons.Count,
+				Time = subTime
+			};
+		}
+
+		static void Test()
+		{
+			var tests = new[] { 10, 50, 100, 150 };
+			var res = tests.Select(TestRes).ToArray();
+			Console.WriteLine("Res,Time,Polygons,Compares");
+			foreach (var r in res)
+			{
+				Console.WriteLine("{0},{1},{2},{3}", r.Resolution, r.Time.TotalSeconds, r.Polygons, r.Compares);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			PrimeJit();
+			Test();
+		}
+	}
+}
