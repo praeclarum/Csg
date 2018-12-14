2e778976125dcd08f0fa4e1eb8ed8a2ea6926bdf
Frank A. Krueger <fak@praeclarum.org>
2018-12-13 16:54:07 -0800

    Move Vertex to its own file

--- Csg/Polygon.cs
+++ Csg/Polygon.cs
@@ -105,42 +105,6 @@ namespace Csg
 		}
 	}
 
-	public class Vertex
-	{
-		public Vector3D Pos;
-		int tag = 0;
-
-		public Vertex(Vector3D pos)
-		{
-			Pos = pos;
-		}
-
-		public int Tag
-		{
-			get
-			{
-				if (tag == 0)
-				{
-					tag = Solid.GetTag();
-				}
-				return tag;
-			}
-		}
-
-		public Vertex Flipped()
-		{
-			return this;
-		}
-
-		public override string ToString() => Pos.ToString();
-
-		public Vertex Transform(Matrix4x4 matrix4x4)
-		{
-			var newpos = Pos * matrix4x4;
-			return new Vertex(newpos);
-		}
-	}
-
 	public class Properties
 	{
 		public readonly Dictionary<string, object> All = new Dictionary<string, object>();
