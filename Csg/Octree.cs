a9c3ad4ff7534d9392198f6ed6f73fd9d1f11fc9
Frank A. Krueger <fak@praeclarum.org>
2018-12-13 17:43:54 -0800

    Make Octree visible to tests

--- Csg/Octree.cs
+++ Csg/Octree.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleToAttribute ("Csg.Test")]
 
 namespace Csg
 {
-	class Octree
+	internal class Octree
 	{
 		public readonly OctreeNode RootNode;
 		public static Octree Unit()
@@ -36,7 +39,7 @@ namespace Csg
 		}
 	}
 
-	class OctreeNode
+	internal class OctreeNode
 	{
 		public readonly BoundingBox BoundingBox;
 		public readonly OctreeNode[] Children;
