42cfb0e77f6cd2d0bcd9cec3f65350d335ef0190
Frank A. Krueger <fak@praeclarum.org>
2018-12-14 18:20:09 -0800

    More doc comments

--- Csg/Vertex.cs
+++ Csg/Vertex.cs
@@ -2,23 +2,32 @@
 {
     public class Vertex
     {
-		/// <summary>
-		/// The world position of this vertex.
-		/// </summary>
+        /// <summary>
+        /// The world position of this vertex.
+        /// </summary>
         public readonly Vector3D Pos;
 
-		/// <summary>
-		/// The texture coordinate of this vertex.
-		/// </summary>
+        /// <summary>
+        /// The texture coordinate of this vertex.
+        /// </summary>
 		public readonly Vector2D Tex;
+
         int tag = 0;
 
+        /// <summary>
+        /// Initializes a new <see cref="T:Csg.Vertex"/> at a given position and with a given texture coordinate.
+        /// </summary>
+        /// <param name="pos">World position</param>
+        /// <param name="tex">Texture coordinate</param>
         public Vertex(Vector3D pos, Vector2D tex)
         {
             Pos = pos;
 			Tex = tex;
         }
 
+        /// <summary>
+        /// A unique id for this vertex.
+        /// </summary>
         public int Tag
         {
             get
@@ -31,6 +40,10 @@
             }
         }
 
+        /// <summary>
+        /// Get a flipped version of this vertex.
+        /// May return the same object if no changes are needed to flip.
+        /// </summary>
         public Vertex Flipped()
         {
             return this;
@@ -38,6 +51,12 @@
 
         public override string ToString() => Pos.ToString();
 
+        /// <summary>
+        /// Left multiplies the position of this vertex with the given matrix.
+        /// The texture coordinate is unchanged.
+        /// </summary>
+        /// <returns>A new transformed vertex.</returns>
+        /// <param name="matrix4x4">The transformation.</param>
         public Vertex Transform(Matrix4x4 matrix4x4)
         {
             var newpos = Pos * matrix4x4;
